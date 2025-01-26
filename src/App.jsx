f79c250c541999713995defdd4d1dcc5f123f3cb
adam/modal <adam@adams-MacBook-Pro.local>
2025-01-26 07:13:19 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ import InsightsResourcesScreen from './screen/adam/InsightsResourcesScreen'
 const App = () => {
   return (
     <div className='bg-[#ffffff] flex flex-col justify-center items-center gap-12 w-full overflow-x-hidden ' >
-      {/* <div className="w-svw h-[850px] rounded-b-[40%] pt-[95px] aspect-square scale-150 -translate-x-3 bg-gradient-to-br from-[#2f91fa] via-[#277ee8] to-[#1449b3] relative"/>
+      <div className="w-svw h-[850px] rounded-b-[40%] pt-[95px] aspect-square scale-150 -translate-x-3 bg-gradient-to-br from-[#2f91fa] via-[#277ee8] to-[#1449b3] relative"/>
         <Navbar styling={'scale-100 absolute z-50 top-2 left-1/2 -translate-x-1/2'}/>
         <MainSection styling={'scale-100 absolute z-40 top-24 left-1/2 -translate-x-1/2'} />
-        <SecondSection/> */}
+        <SecondSection/>
         <InsightsResourcesScreen/>
         <Footer/>
     </div>
